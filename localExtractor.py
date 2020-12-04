c6070611bbbe8570fb8a86090249a9013efd2aa9
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2020-12-03 18:01:27 -0800

    Update localExtractor.py

--- localExtractor.py
+++ localExtractor.py
@@ -25,25 +25,34 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Flatten, Dense, Dropout, GlobalAveragePooling2D
 import wget
+import enum
 
 
 class localExtractor:
     def __init__(self, inputModel, folder, saveFile):
-        self.model=self.extract_features(inputModel,10)
+        self.model=self.__extract_model_till_layer(inputModel,10)
         self.folder=folder
         self.file=saveFile
 
-    def extract_model_till_layer(self,model, layerNo):
-        
+    def __save_features(self,features):
+        t=AnnoyIndex(len(features[0]))
+        for p in range(len(features)):
+            feature = features[p]
+            t.add_item(p, feature)
+
+        t.build(40)  # 40 trees
+        t.save(self.file)
+    def __extract_model_till_layer(self,model, layerNo):
+
         outputs = model.layers[layerNo].output
         model = Model(inputs=model.inputs, outputs=outputs)
         return model
 
-    def extract_features(self,file, model):
-                
-        a = np.asarray(PIL.Image.open(self.folder+file))
+    def __extract_features(self,file, model):
+
+        a = np.asarray(PIL.Image.open('C://Users//abhig//level8images//'+file))
         a=np.resize(a,(1,224,224,3))
-        a=a/255       
+        a=a/255
         #preprocessed_img = preprocess_input(a)
         features = model.predict(a)
         flattened_features = features.flatten()
@@ -51,38 +60,25 @@ class localExtractor:
         return normalized_features
 
     def extract(self):
-                            
+
         all_features=[]
         try:
-            
-        
+
+
             directory=os.listdir(self.folder)
-        
+
             for i in range(0,len(directory)):
-                features=self.extract_features(str(i)+".jpg",self.model)
-                print("length",len(features))
+                features=self.__extract_features(str(i)+".jpg",self.model)
+                print("length",len(feats))
                 all_features.append(features)
                 print(i)
-                
-        
-                
+                counter+=1
 
 
-            # Length of item vector that will be indexed
-            t=AnnoyIndex(len(all_features[0]))
-            for i in range(len(all_features)):
-                feature = all_features[i]
-                t.add_item(i, feature)
 
-            t.build(40)  # 40 trees
-            t.save(self.file)
 
+                # Length of item vector that will be indexed
+                __save_features(all_features)
         except:
-            
-            t=AnnoyIndex(all_features[0])
-            for i in range(len(all_features)):
-                feature = all_features[i]
-                t.add_item(i, feature)
 
-            t.build(40)  # 40 trees
-            t.save(self.file)
+            _save_features(all_features)
